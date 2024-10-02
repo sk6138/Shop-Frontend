95b6484ff6750f4cdc2d4cc9224218e37b651211
sahil kumar <Sk8877532873@gmail.com>
2024-10-02 21:51:39 +0530

    main.jsx is updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react';
+const onRedirectCallback = (appState) => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState?.returnTo || window.location.pathname
+  );
+};
+
 
 createRoot(document.getElementById('root')).render(
  
   <Auth0Provider
   domain="dev-u4zotyqgup2z3tt1.us.auth0.com"
   clientId="hjhhfs9XBpuo5D2Cf6Rhfl4wjcHzuzTZ"
+  redirectUri={window.location.origin}
+  onRedirectCallback={onRedirectCallback}
   authorizationParams={{
     redirect_uri: window.location.origin
   }}
