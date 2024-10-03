30e0da70cd6bcd4abbb00f69e37c1b287b5fe6fd
sahil kumar <Sk8877532873@gmail.com>
2024-10-03 15:42:11 +0530

    commit is revorted

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,11 +6,19 @@ import CardContainer from './CardContainer'
 import Footer from './Footer'
 import ScrollToTop from './ScrollToTop'
 import NotebookCardContainer from './NotebookCardContainer'
+import { useAuth0 } from "@auth0/auth0-react";
 
 
 
 
 export default function Home() {
+  const {
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    error,
+    user
+  } = useAuth0();
  
   return (
     <div>
