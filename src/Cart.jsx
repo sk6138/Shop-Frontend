c58471f03d08e7db33aa6f43fc275c57b3c2b0e9
sahil kumar <Sk8877532873@gmail.com>
2024-10-03 15:25:25 +0530

    commit is revorted

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom';
 import  { useState, useEffect } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 export default function Cart() {
   const [data, setData] = useState([]);
@@ -42,6 +43,35 @@ export default function Cart() {
   
   // const items = data.books;
   console.log(data);
+
+  
+  const removecart = async (id) => {
+    try {
+      const response = await axios.delete(`https://shop-backend-production-d74a.up.railway.app/api/cart/remove/${id}`);
+      console.log('Item deleted:', response.data);
+      Swal.fire({
+        title: 'alert!',
+        text: `${product.productName} has been added to your cart!`,
+        icon: 'success',
+        showConfirmButton: true,
+        timer: 4000, // Auto-close after 2 seconds
+        toast: true, // Make it a small notification
+        position: 'top-right'
+      });
+      
+      // Show success notification or reload the cart items here
+    } catch (error) {
+      console.error('Error deleting the item:', error);
+      // Show error notification here
+    }
+  
+    }
+   
+
+  };
+  
+
+  
   return (
     <div>
       <Navbar/>
@@ -58,7 +88,8 @@ export default function Cart() {
               <p>Quantity: {product.quantity}</p>
               <p>Total Price: ${product.total}</p>
               <img src={product.image} alt={product.name} />
-            
+              <button onClick={removecart((product.id))}>Remove</button>
+              <hr />
             {/* <h2>Total Cart Price: ${product.total}</h2> */}
           </div>
           
@@ -69,4 +100,4 @@ export default function Cart() {
     </div>
     </div>
   )
-}
+
