110330f1fdbfca49735e9b92c73db6623dd3c368
sahil kumar <Sk8877532873@gmail.com>
2024-10-03 14:17:53 +0530

    commit is revorted

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -3,18 +3,48 @@ import Navbar from './Navbar'
 import { useLocation } from 'react-router-dom';
 
 export default function Cart() {
+  const [data, setData] = useState([]);
+  const {
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    user,
+    error,
+  } = useAuth0();
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
-  const { cart } = location.state || { cart: [] };
-  const totalCartPrice = cart.reduce((total, item) => total + item.totalPrice, 0);
-  console.log(cart);
+  // const { cart } = location.state || { cart: [] };
+  // const totalCartPrice = cart.reduce((total, item) => total + item.totalPrice, 0);
+  // console.log(cart);
+  const url =`https://shop-backend-production-d74a.up.railway.app/api/cart/${user.sub}`;
+
+  useEffect(() => {
+    // Call the API when the component mounts https://www.dbooks.org/api/recent
+    axios.get(url)
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching the API', error);
+        setLoading(false);
+      });
+  }, []); // The empty dependency array ensures this runs once on component mount
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  
+  // const items = data.books;
+  console.log(data);
   return (
     <div>
       <Navbar/>
        <div>
       <h1>Cart</h1>
       
-      {cart.length > 0 ? (
-        cart.map((product, index) => (
+      {data.length > 0 ? (
+        data.map((product, index) => (
           <div key={index}>
              <p>Product Name: {product.name}</p>
               <p>Price: ${product.price}</p>
