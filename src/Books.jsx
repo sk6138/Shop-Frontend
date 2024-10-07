0fd1a1948045f926d75c6c548b4e610697755042
sahil kumar <Sk8877532873@gmail.com>
2024-10-07 13:29:17 +0530

    catelog is added and BOOKs also

--- src/Books.jsx
+++ src/Books.jsx
@@ -5,6 +5,7 @@ import Button from './Button'
 import  { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
 
 export default function Books() {
     
@@ -40,6 +41,7 @@ export default function Books() {
 
   return (
     <div>
+        <Navbar/>
        <div className="cardContainer">
       <h3>Featured Books</h3>
       <div className="Feature-card">
