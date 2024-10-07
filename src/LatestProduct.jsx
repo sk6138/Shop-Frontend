67b9073a5fbec44dee70f972e06a1bd1c364cb33
sahil kumar <Sk8877532873@gmail.com>
2024-10-07 23:44:43 +0530

    search button is working

--- src/LatestProduct.jsx
+++ src/LatestProduct.jsx
@@ -5,7 +5,8 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import { useLocation } from 'react-router-dom';
 
-export default function LatestProduct(props) {
+export default function LatestProduct() {
+  const location = useLocation();
   const results = location.state?.results || []; 
   console.log(results);
   return (
