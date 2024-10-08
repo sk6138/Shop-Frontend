5df41604105bb1111adf89739477ce688df918b3
sahil kumar <Sk8877532873@gmail.com>
2024-10-08 10:04:52 +0530

    search button is working

--- src/LatestProduct.jsx
+++ src/LatestProduct.jsx
@@ -44,6 +44,7 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import ProductCard from './ProductCard';
 
 const LatestProduct = () => {
   const location = useLocation();
