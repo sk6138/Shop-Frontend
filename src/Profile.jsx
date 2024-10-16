725fbfcc43ab7247df7eed7b1fd375ab55ba35ef
sahil kumar <Sk8877532873@gmail.com>
2024-10-16 20:30:23 +0530

    login logout

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,35 +1,61 @@
-import React from "react";
+import {React,useContext} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Navbar from "./Navbar";
 import LogoutButton from "./LogoutButton";
 import LoginButton from "./LoginButton";
+import { GlobalContext } from './GlobalContext'; 
+import Footer from "./Footer";
 
-const Profile = () => {
+// import { GlobalContext } from './GlobalProvider';
+// import { useContext ,useState} from "react";
+
+const Profile = (props) => {
+
+  const { globalVar, setGlobalVar } = useContext(GlobalContext);
+  // console.log(globalVar);
+
+   console.log(props.userid);
+  // window.globalThis = sessionStorage.getItem('globalthis'); 
+  //  console.log(window.globalThis);
   const { user, isAuthenticated, isLoading } = useAuth0();
-
+  // const { email, setemail } = useContext(GlobalContext);
+  // setemail(user.email);
+  // console.log(email);
   if (isLoading) {
     return <div>Loading ...</div>;
   }
-   console.log(user);
+   
   return (
     <div>
     <Navbar/>
-
+   
     
-    {isAuthenticated ? (
+  
+
+   
+    
+    {/* {isAuthenticated ? (
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <LogoutButton/>
+        
+        <LoginButton />
+
+
       </div>
+      
       
     ):
     (
       <LoginButton/>
-    )
-  }
+    ) */}
+    
+  {/* } */}
+
+  <LoginButton/>
  
+    <Footer/>
     </div>
   );
 };
